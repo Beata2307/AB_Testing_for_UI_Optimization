396d452d3c163c8d7ad2fb23b3f7b9663c7c9ca4
Beata2307 <beata.taudul@gmail.com>
2024-03-07 21:53:59 +0100

    Functions for data cleaning

--- functions.py
+++ functions.py
@@ -36,17 +36,6 @@ def nr_of_confirms(df):
     return df_client_confirms
 
 
-# def detect_process_errors(df):
-#     """Function that return the dataframe with added columns in order to conduct the error rate analysis"""
-#     df = df.sort_values(['client_id','date_time'])
-#     process_map = {'start':0, 'step_1':1, 'step_2':2, 'step_3':3, 'confirm':4}
-#     df['process_step_id'] = df['process_step'].map(process_map)
-#     df['previous_process_step_id'] = (df.sort_values(by=['client_id','date_time']).groupby('client_id')['process_step_id'].shift(1))
-#     df['is_error'] = df['process_step_id'] < df['previous_process_step_id']
-#     df_client_error = df[df['is_error']==True].groupby('client_id')['is_error'].agg([max,len])
-#     df_client_error.columns = ['had_error','error_count']
-#     return df_client_error
-
 def detect_process_changes(df):
     """Function that return the dataframe with added columns in order to get information on the step changes (progress or step back) in the onboarding process"""
     df = df.sort_values(['client_id','date_time'])
@@ -84,3 +73,14 @@ def nr_of_each_step(df):
     df_client_confirms['no_of_step_3'] = df[['client_id','process_step']][df['process_step']=='step_3'].groupby('client_id').count()   
     df_client_confirms['no_of_confirms'] = df[['client_id','process_step']][df['process_step']=='confirm'].groupby('client_id').count()   
     return df_client_confirms
+
+# def detect_process_errors(df):
+#     """Function that return the dataframe with added columns in order to conduct the error rate analysis"""
+#     df = df.sort_values(['client_id','date_time'])
+#     process_map = {'start':0, 'step_1':1, 'step_2':2, 'step_3':3, 'confirm':4}
+#     df['process_step_id'] = df['process_step'].map(process_map)
+#     df['previous_process_step_id'] = (df.sort_values(by=['client_id','date_time']).groupby('client_id')['process_step_id'].shift(1))
+#     df['is_error'] = df['process_step_id'] < df['previous_process_step_id']
+#     df_client_error = df[df['is_error']==True].groupby('client_id')['is_error'].agg([max,len])
+#     df_client_error.columns = ['had_error','error_count']
+#     return df_client_error
